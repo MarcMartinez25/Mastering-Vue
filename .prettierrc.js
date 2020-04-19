d12d539999756ab8af99e09630bd3227b4f5dc58
Marco Martinez <martinezma025@gmail.com>
2020-04-19 16:07:00 -0500

    all work up to mastering vuex

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    singleQuote: true,
+    semi: true,
+    printWidth: 100,
+    tabWidth: 4,
+    useTabs: false,
+    bracketSpacing: true,
+    trailingComma: 'es5'
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,35 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import EventService from '@/services/EventService.js';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+    state: {
+        user: { id: 'abc123', name: 'Marco Martinez' },
+        categories: [
+            'sustainablility',
+            'nature',
+            'animal welfare',
+            'housing',
+            'education',
+            'food',
+            'community',
+        ],
+        events: [],
+    },
+    mutations: {
+        ADD_EVENT(state, event) {
+            state.events.push(event);
+        },
+    },
+    actions: {
+        CreateEvent({ commit }, event) {
+            return EventService.postEvent(event).then(() => {
+                commit('ADD_EVENT', event);
+            });
+        },
+    },
+    modules: {},
+    getters: {},
 });
